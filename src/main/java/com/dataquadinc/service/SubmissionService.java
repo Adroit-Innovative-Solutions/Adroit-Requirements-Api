29777efc01670024b2917705a1103f81d60a617a
Jayasree710 <dornalajayasree@gmail.com>
2025-11-26 11:47:23 +0530

    added pagination,search,filter for admin-view and self-view. added getBySubmissionId API

--- src/main/java/com/dataquadinc/service/SubmissionService.java
+++ src/main/java/com/dataquadinc/service/SubmissionService.java
@@ -2,20 +2,23 @@ package com.dataquadinc.service;
 
 
 import com.dataquadinc.dtos.SubmissionDTO;
-import com.dataquadinc.model.Submissions;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Map;
 
 public interface SubmissionService {
 
 
     SubmissionDTO createSubmission(String userId, SubmissionDTO submissionDTO, MultipartFile resume) throws IOException;
 
-    List<SubmissionDTO> getSubmission(String userId);
+    SubmissionDTO getSubmissionById(String submissionId);
 
-    List<SubmissionDTO> getSubmissionByTeamLead(String userId);
+    Page<SubmissionDTO> getSubmission(String userId, String keyword, Map<String, Object> filters, Pageable pageable);
+
+    Page<SubmissionDTO> getSubmissionByTeamLead(String userId, String keyword, Map<String, Object> filters, Pageable pageable);
 
     SubmissionDTO updateSubmission(String submissionId, SubmissionDTO submissionDTO, MultipartFile resume);
 
